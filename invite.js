728485b444d850041a58a442ba6b23be2694b1c2
Savannah Israel <sisrael0405@gmail.com>
2019-06-02 05:12:15 -0600

    Added 3rd style plus psuedocode for two remaining styles

--- invite.js
+++ invite.js
@@ -1,9 +1,11 @@
 $("#floralInvite").show();
 $("#foodieInvite").hide();
+$("#modernInvite").hide();
+// $("#bloodyInvite").hide();
+// $("#bubblyInvite").hide();
 
 $("#submit").on("click", function (event) {
     event.preventDefault();
-    //Store User Input
 
     var hostName = $("#hostNameForm").val().trim();
     var eventDate = $("#eventDateForm").val().trim();
@@ -24,16 +26,64 @@ $("#submit").on("click", function (event) {
     $("#eventDateFoodie").html(eventDate);
     $("#eventTimeFoodie").html(eventTime);
     $("#customMessageFoodie").html(customMessage);
+
+    $("#hostNameModern").html(hostName);
+    $("#eventDateModern").html(eventDate);
+    $("#eventTimeModern").html(eventTime);
+    $("#customMessageModern").html(customMessage);
+
+    // $("#hostNameBloody").html(hostName);
+    // $("#eventDateBloody").html(eventDate);
+    // $("#eventTimeBloody").html(eventTime);
+    // $("#customMessageBloody").html(customMessage);
+
+    // $("#hostNameBubbly").html(hostName);
+    // $("#eventDateBubbly").html(eventDate);
+    // $("#eventTimeBubbly").html(eventTime);
+    // $("#customMessageBubbly").html(customMessage);
 })
 
 $("#floralButton").on("click", function (event) {
     event.preventDefault();
     $("#floralInvite").show();
     $("#foodieInvite").hide();
+    $("#modernInvite").hide();
+// $("#bloodyInvite").hide();
+// $("#bubblyInvite").hide();
 });
 
 $("#foodieButton").on("click", function (event) {
     event.preventDefault();
     $("#floralInvite").hide();
     $("#foodieInvite").show();
+    $("#modernInvite").hide();
+// $("#bloodyInvite").hide();
+// $("#bubblyInvite").hide();
 });
+
+$("#modernButton").on("click", function (event) {
+    event.preventDefault();
+    $("#floralInvite").hide();
+    $("#foodieInvite").hide();
+    $("#modernInvite").show();
+// $("#bloodyInvite").hide();
+// $("#bubblyInvite").hide();
+});
+
+// $("#bloodyButton").on("click", function (event) {
+//     event.preventDefault();
+//     $("#floralInvite").hide();
+//     $("#foodieInvite").hide();
+//     $("#modernInvite").hide();
+// $("#bloodyInvite").show();
+// $("#bubblyInvite").hide();
+// });
+
+// $("#bubblyButton").on("click", function (event) {
+//     event.preventDefault();
+//     $("#floralInvite").hide();
+//     $("#foodieInvite").hide();
+//     $("#modernInvite").hide();
+// $("#bloodyInvite").hide();
+// $("#bubblyInvite").show();
+// });
