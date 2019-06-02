619764af811f097221d823877d2dbc5373e2b671
Savannah Israel <sisrael0405@gmail.com>
2019-06-02 04:43:46 -0600

    added mutiple styles and choose style section

--- invite.js
+++ invite.js
@@ -1,7 +1,10 @@
-$("#submit").on("click", function(event){
+$("#floralInvite").show();
+$("#foodieInvite").hide();
+
+$("#submit").on("click", function (event) {
     event.preventDefault();
     //Store User Input
-    // var TrainName = $("#trainname").val().trim();
+
     var hostName = $("#hostNameForm").val().trim();
     var eventDate = $("#eventDateForm").val().trim();
     var eventTime = $("#eventTimeForm").val().trim();
@@ -16,4 +19,21 @@ $("#submit").on("click", function(event){
     $("#eventDate").html(eventDate);
     $("#eventTime").html(eventTime);
     $("#customMessage").html(customMessage);
+
+    $("#hostNameFoodie").html(hostName);
+    $("#eventDateFoodie").html(eventDate);
+    $("#eventTimeFoodie").html(eventTime);
+    $("#customMessageFoodie").html(customMessage);
+})
+
+$("#floralButton").on("click", function (event) {
+    event.preventDefault();
+    $("#floralInvite").show();
+    $("#foodieInvite").hide();
+});
+
+$("#foodieButton").on("click", function (event) {
+    event.preventDefault();
+    $("#floralInvite").hide();
+    $("#foodieInvite").show();
 });
